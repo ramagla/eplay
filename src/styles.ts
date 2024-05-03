83c1895e9abc399aa7559092c92bd84067478a4c
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-05-03 09:13:34 -0300

    finalizada a criação do header

--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
-const Cores = {
+export const Cores = {
   branca: '#eee',
   preta: '#111',
   cinza: '#333',
@@ -13,10 +13,18 @@ export const GlobalCss = createGlobalStyle`
   padding:0;
   box-sizing: border-box;
   font-family: Roboto, sans-serif;
+  list-style: none;
 
   body {
     background-color: ${Cores.preta};
     color: ${Cores.branca};
+    padding-top:40px;
   }
 }
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
 `
