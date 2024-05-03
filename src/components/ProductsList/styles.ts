02b00c7fc1d8281da43d3a367a4a707eccb76044
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-05-03 11:22:44 -0300

    finalizando os cards

--- src/components/ProductsList/styles.ts
+++ src/components/ProductsList/styles.ts
@@ -1,7 +1,18 @@
 import styled from 'styled-components'
 
-export const Container = styled.section`
+import { Props } from '.'
+import { Cores } from '../../styles'
+import { Card } from '../Product/styles'
+
+export const Container = styled.section<Omit<Props, 'title'>>`
   padding: 32px 0;
+  background-color: ${(props) =>
+    props.background === 'black' ? Cores.preta : Cores.cinza};
+
+  ${Card} {
+    background-color: ${(props) =>
+      props.background === 'black' ? Cores.cinza : Cores.preta};
+  }
 `
 
 export const List = styled.ul`
