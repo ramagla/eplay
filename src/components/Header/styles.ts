62b4229b6d40f2b6339cedaa86e02816d26d82bc
Rafael Almeida <dealmeida.silva@hotmail.com>
2024-05-13 12:07:18 -0300

    criando o menu responsivo

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,37 +1,67 @@
 import styled from 'styled-components'
 import { Cores, breakpoints } from '../../styles'
 
+export const Links = styled.ul`
+  display: flex;
+  margin-left: 40px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-left: 0;
+    display: block;
+  }
+`
+
 export const HeaderBar = styled.header`
   background-color: ${Cores.cinza};
   padding: 24px;
   border-radius: 16px;
   margin-bottom: 80px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
 
   a {
     color: ${Cores.branca};
     text-decoration: none;
     font-weight: bold;
   }
+`
+export const HeaderRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-  div {
+  > div {
     display: flex;
     align-items: center;
-  }
 
-  @media (max-width: ${breakpoints.tablet}) {
-    display: none;
+    @media (max-width: ${breakpoints.tablet}) {
+      flex: 1;
+      justify-content: space-between;
+      ${Links} {
+        display: none;
+      }
+    }
   }
 `
-export const Links = styled.ul`
-  display: flex;
-  margin-left: 40px;
+
+export const NavMobile = styled.nav`
+  display: none;
+
+  &.is-open {
+    display: block;
+  }
 `
 
 export const LinkItem = styled.li`
   margin-right: 16px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-right: 0;
+
+    a {
+      display: block;
+      padding: 16px 0;
+      text-align: center;
+    }
+  }
 `
 
 export const CartButton = styled.a`
@@ -39,5 +69,25 @@ export const CartButton = styled.a`
 
   img {
     margin-left: 16px;
+  }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    span {
+      display: none;
+    }
+  }
+`
+export const Hamburger = styled.div`
+  width: 32px;
+  span {
+    height: 2px;
+    display: block;
+    width: 100%;
+    background-color: ${Cores.branca};
+    margin-bottom: 4px;
+  }
+
+  @media (min-width: ${breakpoints.tablet}) {
+    display: none;
   }
 `
